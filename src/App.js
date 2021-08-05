899c728dacbb08e7883be33806563581fc937086
fer peque <fernandopequeno@gmail.com>
2021-08-04 21:33:27 -0300

    3

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="Hello World"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          "Hello World"
         </a>
       </header>
     </div>
