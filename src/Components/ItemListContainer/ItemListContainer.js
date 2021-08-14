6166a46296fe929492309eeb7b2259f8316dd5de
fer peque <fernandopequeno@gmail.com>
2021-08-14 09:38:00 -0300

    Entrega2.2

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './ItemListContainer.css'
 
-export const ItemListContainer = (grettins) => {
+export const ItemListContainer = ({grettins}) => {
 
     return (
-        <div className="Container">
-            <p className="Card">
-                {grettins}
-            </p>
 
+        <div className="Container">
+            <p className="Card"> {grettins} </p>
+            <img src="./img/Rollo.1.jpg" alt=""/>
         </div>    
+
     )
 }
